6f232d1c58a6ca08c9921c9f9110c310621bd83f
816984 <816984@manadulted.ac.uk>
2025-12-11 21:54:11 +0000

    Complete task 2

--- Ex0_revision.py
+++ Ex0_revision.py
@@ -72,6 +72,13 @@ print("\n-------------------------------------------\n"
 # 5. Otherwise (if total_cost is 60 or more), print "Purchase denied: Budget severely exceeded."
 
 # Write your code below:
+budget = 50
+if total_cost <= budget:
+    print("Purchase approved: Within budget.")
+elif total_cost >= budget or budget <= 60:
+    print("Warning: Purchase exceeds budget but is manageable.")
+else:
+    print("Purchase denied: Budget severely exceeded.")
 
 
 # -------------------------------------------
